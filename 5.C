71c1d2e1e7b270e381858a311f8863c9f7068f12
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-22 15:41:41 +0700

    fix algorithm drawline

--- 5.C
+++ 5.C
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
 int drawLine(int size) {
-	int i;
-	int j;
-	int k = 0;
-	for (i=0; i<size; i++) {
-		for (j =0; j < size ; j++) {
-			if (j == k) {
-				printf("*"); 
-			}
-			else {
-				printf(" "); //space berdasarkan banyak size, kalo drawLine(8), jumlah space = 7 dengan 1 bintang (*)
-			} 
-		}
-		k += 1;
-		printf("\n");
-}
+    int j,k;
+
+    for (int i=0; i<size; i++) {
+        if (i >= 1) {
+            printf("%*c", i, ' ');
+        }
+        printf("*\n");
+    }
 }
 
 int main () {
 	drawLine(8); // Menjalankan/Memanggil fungsi, silahkan diganti angkanya untuk di cek
+    return 0;
 }
